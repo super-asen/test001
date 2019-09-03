620a97a44de64896b06b4ef12af5fffeea9c0b0e
asen <1429753895@qq.com>
2019-09-03 12:35:02 +0800

    commit 4

---
+++ src/main/java/com/example/demo0001/bbb.java
@@ -0,0 +1,8 @@
+package com.example.demo0001;
+
+public class bbb {
+    public static void main(String[] args) {
+        System.out.println("0000000000");
+        System.out.println("111111111111");
+    }
+}
